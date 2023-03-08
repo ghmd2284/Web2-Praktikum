e2246d8bc3f75354eaffc88866e175ac309a919e
ghmd2284 <ghomidi.2284@gmail.com>
2023-03-08 18:33:10 +0700

    update

--- Praktikum02/form_bel.php
+++ Praktikum02/form_bel.php
@@ -1,59 +1,130 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title>Belanja Online</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta>
+    <meta http-equiv="X-UA-Compatible"">
+    <meta name=" viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Belanja Online</title>
+
+
+    <!-- isi data inputan -->
+
 <body>
+
     <div class="container">
-        <h2>Belanja Online</h2>
-        <form method="POST" action="form_bel.php">
-            <div class="form-group">
-                <label for="customer">Customer:</label>
-                <input type="text" class="form-control" id="customer" name="customer">
-            </div>
-            <div class="form-group">
-                <label>Pilih Produk:</label>
-                <div class="radio">
-                    <label><input type="radio" name="produk" value="TV">TV: 4.200.000</label>
-                </div>
-                <div class="radio">
-                    <label><input type="radio" name="produk" value="Kulkas">Kulkas: 3.100.000</label>
-                </div>
-                <div class="radio">
-                    <label><input type="radio" name="produk" value="Mesin Cuci">Mesin Cuci: 3.800.000</label>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="jumlah">Jumlah Beli:</label>
-                <input type="text" class="form-control" id="jumlah" name="jumlah">
-            </div>
-            <button type="submit" class="btn btn-default" name="proses">Simpan</button>
-        </form>
+        <h1>Form Belanja</h1>
+        <div class="row">
+            <main class="col-8">
+                <form method="post">
+                    <div class="form-group row">
+                        <label for="customer" class="col-4 col-form-label">Customer</label>
+                        <div class="col-8">
+                            <input id="customer" name="customer" placeholder="nama customer" type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-4">Pilih Produk</label>
+                        <div class="col-8">
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input name="produk" id="radio_0" type="radio" class="custom-control-input" value="TV">
+                                <label for="radio_0" class="custom-control-label">TV</label>
+                            </div>
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input name="produk" id="radio_1" type="radio" class="custom-control-input" value="Kulkas">
+                                <label for="radio_1" class="custom-control-label">Kulkas</label>
+                            </div>
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input name="produk" id="radio_2" type="radio" class="custom-control-input" value="Mesin cuci">
+                                <label for="radio_2" class="custom-control-label">Mesin cuci</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="jumlah" class="col-4 col-form-label">Jumlah</label>
+                        <div class="col-8">
+                            <input id="jumlah" name="jumlah" placeholder="jumlah" type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="offset-4 col-8">
+                            <button name="proses" type="submit" class="btn btn-success">Kirim</button>
+                        </div>
+                </form>
 
-        <?php
-            $customer = $_POST['customer'];
-            $produk = $_POST['produk'];
-            $jumlah = $_POST['jumlah'];
-            $total_harga = 0;
-
-            if($produk == 'TV'){
-                $total_harga = 4200000 * $jumlah;
-            } elseif($produk == 'Kulkas'){
-                $total_harga = 3100000 * $jumlah;
-            } elseif($produk == 'Mesin Cuci'){
-                $total_harga = 3800000 * $jumlah;
-            }
-
-            echo '<br><h4>Hasil:</h4>';
-            echo '<p>Nama Customer: '.$customer.'</p>';
-            echo '<p>Produk Pilihan: '.$produk.'</p>';
-            echo '<p>Jumlah Beli: '.$jumlah.'</p>';
-            echo '<p>Total Belanja: Rp. '.$total_harga.',-</p>';
-        ?>
+            </main>
+            <div class="col-4">
+                <aside>
+                    <table>
+                        <tr>
+                            <td style="background-color: aqua;">Daftar Harga</td>
+                        </tr>
+                        <tr>
+                            <td>TV - Rp. 4.200.000</td>
+                        </tr>
+                        <tr>
+                            <td>Kulkas - Rp. 3.500.000</td>
+                        </tr>
+                        <tr>
+                            <td>Mesin Cuci - Rp. 3.800.000</td>
+                        </tr>
+                        <tr>
+                            <td style="background-color: aqua;"> Harga Dapat Berubah Setiap Saat!</td>
+                        </tr>
+                    </table>
+                </aside>
+            </div>
+        </div>
     </div>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
+
 </html>
+
+<?php
+
+
+// cek kondisi input
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $customer = $_POST['customer'];
+    $produk = $_POST['produk'];
+    $jumlah = $_POST['jumlah'];
+
+    // harga produk
+    $harga_tv = 4200000;
+    $harga_kulkas = 3500000;
+    $harga_mesin_cuci = 3850000;
+
+    // hitung total harga
+    switch ($produk) {
+        case 'TV':
+            $total_harga = $harga_tv * $jumlah;
+            break;
+        case 'Kulkas':
+            $total_harga = $harga_kulkas * $jumlah;
+            break;
+        case 'Mesin cuci':
+            $total_harga = $harga_mesin_cuci * $jumlah;
+            break;
+        default:
+            $total_harga = 0;
+            break;
+    }
+}
+
+// tapilkan hasil inputan
+?>
+<div class="container">
+    <!-- Tampilan Hasil -->
+    <?php
+    if (isset($_POST['proses'])) {
+        echo "Nama Customer : $customer <br>";
+        echo "Produk : $produk <br>";
+        echo "Jumlah Beli : $jumlah <br>";
+        echo "Total Harga : Rp. $total_harga <br>";
+    } else {
+        
+    }
+    ?>
+</div>
